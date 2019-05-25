2ee7d1880cb8ef52b36f489f16af538733a93437
Dylan Wagner <wagner9@pdx.edu>
2019-05-24 20:42:21 -0700

    Create multithread functionality

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,76 @@
 use cpal::*;
+use std::thread;
+use std::sync::mpsc;
+
 fn main() {
-    let default_in = default_input_device().expect("Error finding defalt input");
+    //Set up seperate thread for input and output
+    let (tx, rx) = mpsc::channel();
+    
+
+    thread::spawn(|| {
+        let default_in = default_input_device().expect("Error finding defalt input");
+        let event_loop_in = EventLoop::new();
+        let in_format = default_in.default_input_format().expect("Error getting default input format");
+        let input = event_loop_in.build_input_stream(&default_in, &in_format)
+                .expect("Error building input stream");
+        event_loop_in.play_stream(input.to_owned());
+
+        event_loop_in.run(move |_, stream_data| {
+            match stream_data {
+                StreamData::Input { buffer: UnknownTypeInputBuffer::U16(mut buffer) } => {
+                    for elem in buffer.iter() {
+                        //tx.send(elem).unwrap();
+                    }
+                    //println!();
+                },
+                StreamData::Input { buffer: UnknownTypeInputBuffer::I16(mut buffer) } => {
+                    for elem in buffer.iter() {
+                        //tx.send(elem).unwrap();
+                    }
+                    //println!();
+                },
+                StreamData::Input { buffer: UnknownTypeInputBuffer::F32(mut buffer) } => {
+                    for elem in buffer.iter() {
+                        tx.send(*elem).unwrap();
+                    }
+                    //println!();
+                },
+                _ => (),
+            };
+        });
+    });
+
     let default_out = default_output_device().expect("Error finding defalt output");
-    let event_loop = EventLoop::new();
-
-    let in_format = default_in.default_input_format().expect("Error getting default input format");
+    let event_loop_out = EventLoop::new();
     let out_format = default_out.default_output_format().expect("Error getting default output format");
+    let output = event_loop_out.build_output_stream(&default_out, &out_format)
+            .expect("Failed to build output stream");
+    event_loop_out.play_stream(output);
 
-    let input = event_loop.build_input_stream(&default_in, &in_format).expect("Error building input stream");
-    let output = event_loop.build_output_stream(&default_out, &out_format).expect("Failed to build output stream");
-    event_loop.play_stream(input.to_owned());
-    //event_loop.play_stream(output);
-
-    event_loop.run(move |stream_id, stream_data| {
-        if stream_id == input {
-             match stream_data {
-                 StreamData::Input { buffer: UnknownTypeInputBuffer::U16(mut buffer) } => {
-                     println!("Input buffer size: {}", buffer.len());
-                     for elem in buffer.iter() {
-                         //println!("input {:?}", elem)
-                     }
-                     //println!();
-                 },
-                 StreamData::Input { buffer: UnknownTypeInputBuffer::I16(mut buffer) } => {
-                     println!("Input buffer size: {}", buffer.len());
-                     for elem in buffer.iter() {
-                         //println!("input {:?}", elem)
-                     }
-                     //println!();
-                 },
-                 StreamData::Input { buffer: UnknownTypeInputBuffer::F32(mut buffer) } => {
-                     println!("Input buffer size: {}", buffer.len());
-                     for elem in buffer.iter() {
-                         //println!("input {:?}", elem)
-                     }
-                     //println!();
-                 },
-                 _ => (),
-             };
-
-        } else {
-             match stream_data {
-                 StreamData::Output { buffer: UnknownTypeOutputBuffer::U16(mut buffer) } => {
-                     //println!("Output buffer size: {}", buffer.len());
-                     for elem in buffer.iter_mut() {
-                         //println!("output {:?}", elem)
-                         //*elem = 0;
-                         //println!("output u16");
-                     }
-                     //println!();
-                 },
-                 StreamData::Output { buffer: UnknownTypeOutputBuffer::I16(mut buffer) } => {
-                     //println!("Output buffer size: {}", buffer.len());
-                     for elem in buffer.iter_mut() {
-                         //println!("output {:?}", elem)
-                         //*elem = 0;
-                         //println!("output I16");
-                     }
-                     //println!();
-                 },
-                 StreamData::Output { buffer: UnknownTypeOutputBuffer::F32(mut buffer) } => {
-                     //println!("Output buffer size: {}", buffer.len());
-                     for elem in buffer.iter_mut() {
-                        //println!("output {:?}", elem)
-                        //*elem = 0.0;
-                        //println!("output F32 {}", elem);
-                     }
-                     //println!();
-                 },
-                 _ => (),
-             };
-        }
+    event_loop_out.run(move |_, stream_data| {
+        match stream_data {
+            StreamData::Output { buffer: UnknownTypeOutputBuffer::U16(mut buffer) } => {
+                for elem in buffer.iter_mut() {
+                    //*elem = *rx.recv().unwrap();
+                    println!("output {:?}", elem);
+                }
+                //println!();
+            },
+            StreamData::Output { buffer: UnknownTypeOutputBuffer::I16(mut buffer) } => {
+                for elem in buffer.iter_mut() {
+                    //*elem = *rx.recv().unwrap();
+                    println!("output {:?}", elem);
+                }
+                //println!();
+            },
+            StreamData::Output { buffer: UnknownTypeOutputBuffer::F32(mut buffer) } => {
+                for elem in buffer.iter_mut() {
+                    *elem *= -rx.recv().unwrap();
+                    println!("output {:?}", elem);
+                }
+                //println!();
+            },
+            _ => (),
+        };
     });
 }

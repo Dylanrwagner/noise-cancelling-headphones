ca0e51f81ec35771da6a3352839aa9e34744b3cb
Dylan Wagner <wagner9@pdx.edu>
2019-06-03 15:56:20 -0700

    calculate amplitude

--- src/calibrate/mod.rs
+++ src/calibrate/mod.rs
@@ -1,5 +1,76 @@
+use cpal::*;
+use std::thread;
+use std::sync::*;
+
 //Calculate the delay caused by the system, and the amplitude of the sound that leaks
-// into the headphones
+// into the headphones. Return a tuple containing the amplitude and the delay
+fn calibrate(def_in: Device, calibrator: Device, def_out: Device) -> (f32, usize) {
+    let amplitude: f32 = calc_amplitude(&calibrator);
+    let delay: usize = calc_delay(def_in, calibrator, def_out);
+    (amplitude, delay)
+}
+
+//Find amplitude of sound leaking in to the headphones
+fn calc_amplitude(calibrator: &Device) -> f32 {
+    let event_loop = EventLoop::new();
+    let buf: Arc<Mutex<Vec<f32>>> = Arc::new(Mutex::new(Vec::new()));
+    //Idea for atomic bool for tracking whether a thread should continue borrowed
+    // from cpal's "record_wav" example.
+    //Original: https://github.com/tomaka/cpal/blob/master/examples/record_wav.rs
+    let calibrating: Arc<atomic::AtomicBool> = Arc::new(atomic::AtomicBool::new(true));
+
+    let buf_clone = buf.clone();
+    let calibrating_clone = calibrating.clone();
+    let format = calibrator.default_input_format().expect("Error getting default input format");
+    let input = event_loop.build_input_stream(&calibrator, &format)
+            .expect("Error building input stream");
+    //Collect samples to find amplitude
+    event_loop.play_stream(input.to_owned());
+    thread::spawn(move || {
+        event_loop.run(move |_, stream_data| {
+            if !calibrating_clone.load(atomic::Ordering::Relaxed) { return; }
+            //catch input in u16, i16, or f32 format
+            match stream_data {
+                StreamData::Input { buffer: UnknownTypeInputBuffer::U16(buffer) } => {
+                    for elem in buffer.iter() {
+                        buf_clone.lock().unwrap().push(*elem as f32);
+                    }
+                },
+                StreamData::Input { buffer: UnknownTypeInputBuffer::I16(buffer) } => {
+                    for elem in buffer.iter() {
+                        buf_clone.lock().unwrap().push(*elem as f32);
+                    }
+                },
+                StreamData::Input { buffer: UnknownTypeInputBuffer::F32(buffer) } => {
+                    for elem in buffer.iter() {
+                        buf_clone.lock().unwrap().push(*elem);
+                    }
+                },
+                _ => println!("no input"),
+            };
+        });
+
+    });
+
+    //collect samples for some time
+    thread::sleep(std::time::Duration::from_secs(1));
+    calibrating.store(false, atomic::Ordering::Relaxed);
+
+    //find peak-to-peak amplitude of recorded sound
+    let mut peak_to_peak: f32 = buf.lock().unwrap().iter().fold(-std::f32::INFINITY, |max, &item| {
+        if max <= item { item }
+        else { max }
+    });
+    peak_to_peak -= buf.lock().unwrap().iter().fold(std::f32::INFINITY, |min, &item| {
+        if min >= item { item }
+        else { min }
+    });
+    peak_to_peak / 2.0_f32.sqrt()
+}
+
+fn calc_delay(def_in: Device, cali: Device, def_out: Device) -> usize {
+    unimplemented!("calculate delay");
+}
 
 /// Find the local maxima of the given vector. Return the numbers and
 /// their indices
--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,6 @@ fn main() {
 
     //set up default input device, which will act as the means to cancel the noise
     let default_in = default_input_device().expect("Error finding default input");
-    let in_formats = default_in.supported_input_formats().unwrap();
 
     //A second input device inside the headphones to find some noise-cancelling parameters
     // assumes it, the default input device, and the built-in mic are the only input devices
@@ -26,6 +25,20 @@ fn main() {
         .unwrap();
 
     println!("calibrator name = {}", calibrator.name());
+    println!("supported default input formats: ");
+    for i in default_in.supported_input_formats().unwrap() {
+        println!("\tmax_sample_rate: {:?} min_sample_rate: {:?} data_type {:?}", i.max_sample_rate,
+            i.min_sample_rate, i.data_type);
+    }
+
+    println!("supported calibrator input formats: ");
+    for i in calibrator.supported_input_formats().unwrap() {
+        println!("\tmax_sample_rate: {:?} min_sample_rate: {:?} data_type {:?}", i.max_sample_rate,
+            i.min_sample_rate, i.data_type);
+    }
+    //let (in_format, cali_format) = match_rates(default_in, calibrator);
+    //println!("Default input: sample rate {:?}, type {:?}", in_format.max_sample_rate, in_format.data_type);
+    //println!("Calibrator: sample rate {:?}, type {:?}", cali_format.max_sample_rate, cali_format.data_type);
 
     //Spawn thread that reads input and sends it to output thread
     //thread::spawn(|| {
@@ -121,3 +134,42 @@ fn main() {
     //    buf.push_back(rx.recv().expect("receive failed on push in event loop"));
     //});
 }
+
+//Find sample rates that work for default input and calibrator devices
+// returns a tuple for the default and calibrator formats, respectively
+//fn match_rates(def_in: Device, cali: Device) -> (SupportedFormat, SupportedFormat) {
+//    //Find calibrator formats which the default format can match
+//    let mut usable_cali_formats = cali.supported_input_formats()
+//        .expect("failed to get supported formats (line 132)")
+//        .filter(|x| { 
+//        //else { return false; }
+//        let in_formats = def_in.supported_input_formats().expect("failed to get supported formats (line 136)");
+//        if in_formats
+//            .filter(|y| y.max_sample_rate == x.max_sample_rate)
+//            .collect::<Vec<SupportedFormat>>()
+//            .len() != 0 {
+//            return true;
+//        }
+//        return false;
+//    });
+//
+//    //Find the best usable calibrator format (highest sample rate)
+//    let mut max_rate = usable_cali_formats.next().expect("failed to get next usable format");
+//    for i in usable_cali_formats {
+//        if i.max_sample_rate > max_rate.max_sample_rate {
+//            max_rate = i;
+//        }
+//    }
+//
+//    let cali_format = max_rate;
+//
+//    //match selected calibrator rate for default input. find f32
+//    let in_format = def_in.supported_input_formats()
+//        .expect("failed to get supported formats")
+//        .find(|x| {
+//        if x.max_sample_rate != cali_format.max_sample_rate { return false }
+//        false
+//    })
+//    .expect("No usable in_format");
+//    (in_format, cali_format)
+//}

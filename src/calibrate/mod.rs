ab0a24fba90d259ba0cc074b317df0cbbe13161f
Dylan Wagner <wagner9@pdx.edu>
2019-06-06 15:01:03 -0700

    echo at a delay

--- src/calibrate/mod.rs
+++ src/calibrate/mod.rs
@@ -2,16 +2,11 @@ use cpal::*;
 use std::thread;
 use std::sync::*;
 
-//Calculate the delay caused by the system, and the amplitude of the sound that leaks
-// into the headphones. Return a tuple containing the amplitude and the delay
-fn calibrate(def_in: Device, calibrator: Device, def_out: Device) -> (f32, usize) {
-    let amplitude: f32 = calc_amplitude(&calibrator);
-    let delay: usize = calc_delay(def_in, calibrator, def_out);
-    (amplitude, delay)
-}
+//Duration to calibrate for for each parameter
+const DURATION: std::time::Duration = std::time::Duration::from_secs(1);
 
 //Find amplitude of sound leaking in to the headphones
-fn calc_amplitude(calibrator: &Device) -> f32 {
+pub fn calc_amplitude(calibrator: &Device) -> f32 {
     let event_loop = EventLoop::new();
     let buf: Arc<Mutex<Vec<f32>>> = Arc::new(Mutex::new(Vec::new()));
     //Idea for atomic bool for tracking whether a thread should continue borrowed
@@ -25,7 +20,7 @@ fn calc_amplitude(calibrator: &Device) -> f32 {
     let input = event_loop.build_input_stream(&calibrator, &format)
             .expect("Error building input stream");
     //Collect samples to find amplitude
-    event_loop.play_stream(input.to_owned());
+    event_loop.play_stream(input);
     thread::spawn(move || {
         event_loop.run(move |_, stream_data| {
             if !calibrating_clone.load(atomic::Ordering::Relaxed) { return; }
@@ -53,7 +48,7 @@ fn calc_amplitude(calibrator: &Device) -> f32 {
     });
 
     //collect samples for some time
-    thread::sleep(std::time::Duration::from_secs(1));
+    thread::sleep(DURATION);
     calibrating.store(false, atomic::Ordering::Relaxed);
 
     //find peak-to-peak amplitude of recorded sound
@@ -68,39 +63,35 @@ fn calc_amplitude(calibrator: &Device) -> f32 {
     peak_to_peak / 2.0_f32.sqrt()
 }
 
-fn calc_delay(def_in: Device, cali: Device, def_out: Device) -> usize {
-    unimplemented!("calculate delay");
-}
-
-/// Find the local maxima of the given vector. Return the numbers and
-/// their indices
-fn local_maxima(input: Vec<f32>) -> Vec<(f32, usize)> {
-    if input.len() == 0 {
-        return Vec::new();
-    }
-    else if input.len() == 1 {
-        return [(input[0], 0)].to_vec();
-    }
-    let mut result = Vec::new();
-    if input[0] >= input[1] {
-        result.push((input[0], 0));
-    }
-    for i in 1..input.len()-1 {
-        if input[i] >= input[i-1] && input[i] >= input[i+1] {
-            result.push((input[i], i));
-        }
-    }
-    if input[input.len()-1] >= input[input.len()-2] {
-        result.push((input[input.len()-1], input.len()-1));
-    }
-    result
-}
-
-#[test]
-fn test_local_maxima() {
-    assert_eq!(local_maxima(Vec::new()), Vec::new());
-    assert_eq!(local_maxima([1.0,3.0,2.0].to_vec()), [(3.0, 1)].to_vec());
-    assert_eq!(local_maxima([5.0,2.0].to_vec()), [(5.0,0)].to_vec());
-    assert_eq!(local_maxima([1.0].to_vec()), [(1.0,0)].to_vec());
-    assert_eq!(local_maxima([1.0, 3.0, 3.0, 2.0].to_vec()), [(3.0,1),(3.0,2)].to_vec());
-}
+///// Find the local maxima of the given vector. Return the numbers and
+///// their indices
+//fn local_maxima(input: Vec<f32>) -> Vec<(f32, usize)> {
+//    if input.len() == 0 {
+//        return Vec::new();
+//    }
+//    else if input.len() == 1 {
+//        return [(input[0], 0)].to_vec();
+//    }
+//    let mut result = Vec::new();
+//    if input[0] >= input[1] {
+//        result.push((input[0], 0));
+//    }
+//    for i in 1..input.len()-1 {
+//        if input[i] >= input[i-1] && input[i] >= input[i+1] {
+//            result.push((input[i], i));
+//        }
+//    }
+//    if input[input.len()-1] >= input[input.len()-2] {
+//        result.push((input[input.len()-1], input.len()-1));
+//    }
+//    result
+//}
+//
+//#[test]
+//fn test_local_maxima() {
+//    assert_eq!(local_maxima(Vec::new()), Vec::new());
+//    assert_eq!(local_maxima([1.0,3.0,2.0].to_vec()), [(3.0, 1)].to_vec());
+//    assert_eq!(local_maxima([5.0,2.0].to_vec()), [(5.0,0)].to_vec());
+//    assert_eq!(local_maxima([1.0].to_vec()), [(1.0,0)].to_vec());
+//    assert_eq!(local_maxima([1.0, 3.0, 3.0, 2.0].to_vec()), [(3.0,1),(3.0,2)].to_vec());
+//}
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::collections::VecDeque;
 
 mod calibrate;
 
-const BUF_SIZE: usize = 1024;
+const DELAY: f32 = 0.18; //Delay in seconds, found through experimentation
 
 ///Take audio from the device's current default input and output a phase-reversed
 /// version to default audio output, creating a noise-cancelling effect. 
@@ -13,163 +13,117 @@ const BUF_SIZE: usize = 1024;
 
 fn main() {
     //Set up seperate thread for input and output
-    //let (tx, rx) = mpsc::channel();
+    let (tx, rx) = mpsc::channel();
 
     //set up default input device, which will act as the means to cancel the noise
     let default_in = default_input_device().expect("Error finding default input");
+    let in_format = default_in.default_input_format().expect("Error getting default input format");
+    let SampleRate(samp_rate) = in_format.sample_rate;
 
     //A second input device inside the headphones to find some noise-cancelling parameters
     // assumes it, the default input device, and the built-in mic are the only input devices
     let calibrator = input_devices()
         .find(|x| { x.name() != "Built-in Microphone" && *x != default_in})
         .unwrap();
-
-    println!("calibrator name = {}", calibrator.name());
-    println!("supported default input formats: ");
-    for i in default_in.supported_input_formats().unwrap() {
-        println!("\tmax_sample_rate: {:?} min_sample_rate: {:?} data_type {:?}", i.max_sample_rate,
-            i.min_sample_rate, i.data_type);
-    }
-
-    println!("supported calibrator input formats: ");
-    for i in calibrator.supported_input_formats().unwrap() {
-        println!("\tmax_sample_rate: {:?} min_sample_rate: {:?} data_type {:?}", i.max_sample_rate,
-            i.min_sample_rate, i.data_type);
-    }
-    //let (in_format, cali_format) = match_rates(default_in, calibrator);
-    //println!("Default input: sample rate {:?}, type {:?}", in_format.max_sample_rate, in_format.data_type);
-    //println!("Calibrator: sample rate {:?}, type {:?}", cali_format.max_sample_rate, cali_format.data_type);
+    print!("calibrating...  ");
+    let amp = calibrate::calc_amplitude(&calibrator);
+    println!("done");
 
     //Spawn thread that reads input and sends it to output thread
-    //thread::spawn(|| {
-    //    let event_loop_in = EventLoop::new();
-    //    let in_format = default_in.default_input_format().expect("Error getting default input format");
-    //    let input = event_loop_in.build_input_stream(&default_in, &in_format)
-    //            .expect("Error building input stream");
-    //    event_loop_in.play_stream(input.to_owned());
+    thread::spawn(move || {
+        let event_loop_in = EventLoop::new();
+        let input = event_loop_in.build_input_stream(&default_in, &in_format)
+                .expect("Error building input stream");
+        event_loop_in.play_stream(input.to_owned());
 
-    //    event_loop_in.run(move |_, stream_data| {
-    //        //catch input in u16, i16, or f32 format
-    //        match stream_data {
-    //            StreamData::Input { buffer: UnknownTypeInputBuffer::U16(buffer) } => {
-    //                for elem in buffer.iter() {
-    //                    tx.send(*elem as f32).expect("input thread failed on send u16");
-    //                }
-    //                //println!("input U16");
-    //                //println!();
-    //            },
-    //            StreamData::Input { buffer: UnknownTypeInputBuffer::I16(buffer) } => {
-    //                for elem in buffer.iter() {
-    //                    tx.send(*elem as f32).expect("input thread failed on send i16");
-    //                }
-    //                //println!("input I16");
-    //                //println!();
-    //            },
-    //            StreamData::Input { buffer: UnknownTypeInputBuffer::F32(buffer) } => {
-    //                for elem in buffer.iter() {
-    //                    tx.send(*elem).expect("input thread failed on send f32");
-    //                }
-    //                //println!("input F32");
-    //                //println!();
-    //            },
-    //            _ => println!("no input"),
-    //        };
-    //    });
-    //});
+        event_loop_in.run(move |_, stream_data| {
+            //catch input in u16, i16, or f32 format
+            match stream_data {
+                StreamData::Input { buffer: UnknownTypeInputBuffer::U16(buffer) } => {
+                    for elem in buffer.iter() {
+                        tx.send(*elem as f32).expect("input thread failed on send u16");
+                    }
+                    println!("input U16");
+                    //println!();
+                },
+                StreamData::Input { buffer: UnknownTypeInputBuffer::I16(buffer) } => {
+                    for elem in buffer.iter() {
+                        tx.send(*elem as f32).expect("input thread failed on send i16");
+                    }
+                    println!("input I16");
+                    //println!();
+                },
+                StreamData::Input { buffer: UnknownTypeInputBuffer::F32(buffer) } => {
+                    for elem in buffer.iter() {
+                        tx.send(*elem).expect("input thread failed on send f32");
+                    }
+                    println!("input F32");
+                    //println!();
+                },
+                _ => println!("no input"),
+            };
+        });
+    });
 
-    //let mut buf: VecDeque<f32> = VecDeque::new(); //Buffer of input samples
-    ////Build buffer up to input size
-    //for _ in 0..BUF_SIZE {
-    //    buf.push_back(rx.recv().expect("failed to build buffer"));
-    //}
+    //Number of samples to delay for
+    let delay_samples = (DELAY * samp_rate as f32).floor() as usize;
 
-    //let default_out = default_output_device().expect("Error finding default output");
-    //let event_loop_out = EventLoop::new();
-    //let out_format = default_out.default_output_format().expect("Error getting default output format");
-    //let output = event_loop_out.build_output_stream(&default_out, &out_format)
-    //        .expect("Failed to build output stream");
-    //event_loop_out.play_stream(output);
+    let mut buf: VecDeque<f32> = VecDeque::new(); //Buffer of input samples
+    //Build buffer up to input size
+    for _ in 0..delay_samples {
+        buf.push_back(rx.recv().expect("failed to build buffer"));
+    }
 
-    //event_loop_out.run(move |_, stream_data| {
-    //    //Output audio in u16, i16, or f32 format
-    //    match stream_data {
-    //        StreamData::Output { buffer: UnknownTypeOutputBuffer::U16(mut buffer) } => {
-    //            for elem in buffer.iter_mut() {
-    //                match buf.pop_front() {
-    //                    Some(i) => *elem = -i as u16,
-    //                    None => (), //Silently drop the sample if buffer is empty (not sure why this is happening)
-    //                }
-    //                //*elem = -buf.pop_front().expect("pop failed in u16") as u16;
-    //                //*elem = -rx.recv().expect("pop failed in u16") as u16;
-    //                //println!("output {:?}", elem);
-    //            }
-    //            //println!("Output u16");
-    //        },
-    //        StreamData::Output { buffer: UnknownTypeOutputBuffer::I16(mut buffer) } => {
-    //            for elem in buffer.iter_mut() {
-    //                match buf.pop_front() {
-    //                    Some(i) => *elem = -i as i16,
-    //                    None => (),
-    //                }
-    //                //*elem = -buf.pop_front().expect("pop failed in i16") as i16;
-    //                //*elem = -rx.recv().expect("pop failed in u16") as i16;
-    //                //println!("output {:?}", elem);
-    //            }
-    //            //println!("Output i16");
-    //        },
-    //        StreamData::Output { buffer: UnknownTypeOutputBuffer::F32(mut buffer) } => {
-    //            for elem in buffer.iter_mut() {
-    //                match buf.pop_front() {
-    //                    Some(i) => *elem = -i,
-    //                    None => (),
-    //                }
-    //                //*elem = -buf.pop_front().expect("pop failed in f32 section");
-    //                //*elem = -rx.recv().expect("pop failed in u16");
-    //                //println!("output {:?}", elem);
-    //            }
-    //            //println!("output f32");
-    //        },
-    //        _ => println!("no output"),
-    //    };
-    //    buf.push_back(rx.recv().expect("receive failed on push in event loop"));
-    //});
+    let default_out = default_output_device().expect("Error finding default output");
+    let event_loop_out = EventLoop::new();
+    let out_format = default_out.default_output_format().expect("Error getting default output format");
+    let output = event_loop_out.build_output_stream(&default_out, &out_format)
+            .expect("Failed to build output stream");
+    event_loop_out.play_stream(output);
+
+    event_loop_out.run(move |_, stream_data| {
+        //Output audio in u16, i16, or f32 format
+        match stream_data {
+            StreamData::Output { buffer: UnknownTypeOutputBuffer::U16(mut buffer) } => {
+                for elem in buffer.iter_mut() {
+                    match buf.pop_front() {
+                        Some(i) => *elem = (-i * amp) as u16,
+                        None => *elem = 0, //Silently drop the sample if buffer is empty 
+                    }
+                    buf.push_back(rx.recv().expect("receive failed on push in event loop"));
+                    //*elem = -buf.pop_front().expect("pop failed in u16") as u16;
+                    //*elem = -rx.recv().expect("pop failed in u16") as u16;
+                    //println!("output {:?}", elem);
+                }
+                println!("Output u16");
+            },
+            StreamData::Output { buffer: UnknownTypeOutputBuffer::I16(mut buffer) } => {
+                for elem in buffer.iter_mut() {
+                    match buf.pop_front() {
+                        Some(i) => *elem = (-i * amp) as i16,
+                        None => (),
+                    }
+                    buf.push_back(rx.recv().expect("receive failed on push in event loop"));
+                    //*elem = -buf.pop_front().expect("pop failed in i16") as i16;
+                    //*elem = -rx.recv().expect("pop failed in u16") as i16;
+                    //println!("output {:?}", elem);
+                }
+                println!("Output i16");
+            },
+            StreamData::Output { buffer: UnknownTypeOutputBuffer::F32(mut buffer) } => {
+                for elem in buffer.iter_mut() {
+                    match buf.pop_front() {
+                        Some(i) => *elem = -i * amp,
+                        None => println!("no input"),
+                    }
+                    buf.push_back(rx.recv().expect("receive failed on push in event loop"));
+                    //*elem = -buf.pop_front().expect("pop failed in f32 section");
+                    //*elem = -rx.recv().expect("pop failed in u16");
+                    //println!("output {:?}", elem);
+                }
+                println!("output f32");
+            },
+            _ => println!("no output"),
+        };
+    });
 }
-
-//Find sample rates that work for default input and calibrator devices
-// returns a tuple for the default and calibrator formats, respectively
-//fn match_rates(def_in: Device, cali: Device) -> (SupportedFormat, SupportedFormat) {
-//    //Find calibrator formats which the default format can match
-//    let mut usable_cali_formats = cali.supported_input_formats()
-//        .expect("failed to get supported formats (line 132)")
-//        .filter(|x| { 
-//        //else { return false; }
-//        let in_formats = def_in.supported_input_formats().expect("failed to get supported formats (line 136)");
-//        if in_formats
-//            .filter(|y| y.max_sample_rate == x.max_sample_rate)
-//            .collect::<Vec<SupportedFormat>>()
-//            .len() != 0 {
-//            return true;
-//        }
-//        return false;
-//    });
-//
-//    //Find the best usable calibrator format (highest sample rate)
-//    let mut max_rate = usable_cali_formats.next().expect("failed to get next usable format");
-//    for i in usable_cali_formats {
-//        if i.max_sample_rate > max_rate.max_sample_rate {
-//            max_rate = i;
-//        }
-//    }
-//
-//    let cali_format = max_rate;
-//
-//    //match selected calibrator rate for default input. find f32
-//    let in_format = def_in.supported_input_formats()
-//        .expect("failed to get supported formats")
-//        .find(|x| {
-//        if x.max_sample_rate != cali_format.max_sample_rate { return false }
-//        false
-//    })
-//    .expect("No usable in_format");
-//    (in_format, cali_format)
-//}
